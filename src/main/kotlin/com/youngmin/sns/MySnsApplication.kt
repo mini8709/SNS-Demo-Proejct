b7374a29167cdabdff507dc6eafebb08459bcc80
Youngmin <mini8709@naver.com>
2025-10-08 13:27:04 +0900

    init: 프로젝트 시작

---
+++ src/main/kotlin/com/youngmin/sns/MySnsApplication.kt
@@ -0,0 +1,11 @@
+package com.youngmin.sns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySnsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MySnsApplication>(*args)
+}
---
+++ src/test/kotlin/com/youngmin/sns/MySnsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youngmin.sns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySnsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
